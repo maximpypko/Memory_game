ea7de7722c172ba681ee0babaf23f1dea275953b
Maxim Pypko <max-rewq@rambler.ru>
2021-04-12 23:09:19 +0300

    added field selection events, styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,27 @@
-console.log('hello');
+function showsForm() {
+    const $header = document.querySelector('.header')
+    $header.style.opacity = 1;
+    $header.style.transition = '1s ease-in-out'
+}
+setTimeout(function(){showsForm();},4000);
+
+(function chooseSizeField() {
+    const $containerSizesField = document.querySelector('.header');
+
+    $containerSizesField.addEventListener('click', (e) => {
+
+        if (e.target.tagName === 'LI') {
+            const arrSizesFields = document.querySelectorAll('.list__item')
+            const $startButton = document.querySelector('.header__button')
+
+            arrSizesFields.forEach(el => {
+                if (el.closest('.list__item--active')) {
+                    el.classList.remove('list__item--active');
+                }
+            })
+
+            e.target.classList.add('list__item--active');
+            $startButton.classList.add('header__button--active')
+        }
+    })
+})()
