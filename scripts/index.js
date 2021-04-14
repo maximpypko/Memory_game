fb8a253d90df8a78d17de67604be7371233d8ac8
Maxim Pypko <max-rewq@rambler.ru>
2021-04-14 21:30:07 +0300

    placed the cards in the container, added icons

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,73 @@
-function showsForm() {
-    const $header = document.querySelector('.header')
-    $header.style.opacity = 1;
-    $header.style.transition = '1s ease-in-out'
-}
-setTimeout(function(){showsForm();},4000);
+class Game {
+    constructor(size) {
+        this.size = size;
+        this.showsForm();
+        this.chooseSizeField();
+    }
+    
+    showsForm() {
+        setTimeout(function () {
+            const $header = document.querySelector('.header')
+            $header.style.opacity = 1;
+            $header.style.transition = '1.5s ease-in-out'
+        },3000);
+    }
+    
+    chooseSizeField() {
+        const $containerSizesField = document.querySelector('.header');
 
-(function chooseSizeField() {
-    const $containerSizesField = document.querySelector('.header');
-
-    $containerSizesField.addEventListener('click', (e) => {
-
-        if (e.target.tagName === 'LI') {
-            const arrSizesFields = document.querySelectorAll('.list__item')
+        $containerSizesField.addEventListener('click', (e) => {
             const $startButton = document.querySelector('.header__button')
+    
+            if (e.target.tagName === 'LI') {
+                const arrSizesFields = document.querySelectorAll('.list__item')
+                
+                arrSizesFields.forEach(el => {
+                    if (el.closest('.list__item--active')) {
+                        el.classList.remove('list__item--active');
+                    }
+                })
+    
+                e.target.classList.add('list__item--active');
+                $startButton.classList.add('header__button--active')
+    
+            } else if (e.target.closest('.header__button--active')) {
+                $startButton.classList.remove('header__button--active');
+    
+                const activeSizeItem = document.querySelector('.list__item--active');
+                const size = activeSizeItem.textContent;
+                this.convertsToNumbers(size)
 
-            arrSizesFields.forEach(el => {
-                if (el.closest('.list__item--active')) {
-                    el.classList.remove('list__item--active');
-                }
-            })
+                const $header = document.querySelector('.header')
+                $header.style.opacity = 0;
+            }
+        })
+    }
 
-            e.target.classList.add('list__item--active');
-            $startButton.classList.add('header__button--active')
+    convertsToNumbers(size) {
+        const [a, b] = size.split('*');
+        this.showCards(+a, +b);
+    }
+
+    showCards(a, b) {
+        const $cards__list = document.createElement('ul')
+        $cards__list.classList.add('cards__list')
+        const quantityCards = a * b;
+
+        for (let index = 0; index < quantityCards; index++) {
+            const $li = document.createElement('li')
+            $li.classList.add('cards__item')
+            $li.innerHTML = `<img src = '' alt = 'card image' class = 'card__image'`
+            $cards__list.append($li)
+
+            $li.style.width = `${95 / a}%`;
+            $li.style.height = `${90 / b}%`;
         }
-    })
-})()
+
+        const $content = document.querySelector('.content')
+        $content.style.display = 'block'
+        $content.append($cards__list)
+    }
+}
+
+new Game();
