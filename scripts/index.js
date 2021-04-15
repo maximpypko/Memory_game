aac57b484493d0449a4aff7b4de225e42da6531e
Maxim Pypko <max-rewq@rambler.ru>
2021-04-16 00:35:03 +0300

    methods disposeIcons and getRandomArray, changed icons, added styles to them

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,18 @@
 class Game {
+
+    icons = [
+        './images/1.png',
+        './images/2.png',
+        './images/3.png',
+        './images/4.png',
+        './images/5.png',
+        './images/6.png',
+        './images/7.png',
+        './images/8.png',
+        './images/9.png',
+        './images/10.png',
+    ]
+
     constructor(size) {
         this.size = size;
         this.showsForm();
@@ -52,21 +66,67 @@ class Game {
     showCards(a, b) {
         const $cards__list = document.createElement('ul')
         $cards__list.classList.add('cards__list')
+
         const quantityCards = a * b;
 
         for (let index = 0; index < quantityCards; index++) {
-            const $li = document.createElement('li')
-            $li.classList.add('cards__item')
-            $li.innerHTML = `<img src = '' alt = 'card image' class = 'card__image'`
-            $cards__list.append($li)
 
-            $li.style.width = `${95 / a}%`;
-            $li.style.height = `${90 / b}%`;
+            const $cardsItem = document.createElement('li')
+            $cardsItem.classList.add('cards__item')
+            $cards__list.append($cardsItem)
+            $cardsItem.style.width = `${95 / a}%`;
+            $cardsItem.style.height = `${90 / b}%`;
         }
 
         const $content = document.querySelector('.content')
         $content.style.display = 'block'
         $content.append($cards__list)
+
+        this.disposeIcons()
+    }
+
+    disposeIcons() {
+        
+        const arrCard = document.querySelectorAll('.cards__item')
+        const randomArray = this.getRandomArray(arrCard.length, 0, arrCard.length)
+
+        function qwerty(icons, randomA , count = 0, count2 = 0) {
+
+            arrCard[randomA[count2]].insertAdjacentHTML('afterbegin',
+                `<img src = '${icons[count]}' alt = 'card icon' class = 'card__item--icon'>`
+            )
+            arrCard[randomA[++count2]].insertAdjacentHTML('afterbegin',
+                `<img src = '${icons[count]}' alt = 'card icon' class = 'card__item--icon'>`
+            )
+            
+            count++;
+            count2++
+
+            if (count === 10) {
+                count = 0;
+            }
+            if (count2 === randomA.length) {
+                return
+            } else {
+                qwerty(icons, randomA, count, count2)
+            }
+        }
+        qwerty(this.icons, randomArray)
+
+    }
+
+    getRandomArray(count, min, max){
+        if (count > (max - min)) return;
+        let randomArray = [], num;
+      
+        while (count) {
+            num = Math.floor(Math.random() * (max - min) + min);
+            if (randomArray.indexOf(num) === -1) {
+                randomArray.push(num);
+                count--;
+            } 
+        }
+        return randomArray;
     }
 }
 
